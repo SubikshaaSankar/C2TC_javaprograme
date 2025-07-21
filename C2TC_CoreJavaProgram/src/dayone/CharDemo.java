b785de0925611f3d08dc78d2ee63421f3e04183e
Asus <Asus@TUF-F15>
2025-07-21 22:02:01 +0530

    this is my chardemo program

---
+++ C2TC_CoreJavaProgram/src/dayone/CharDemo.java
@@ -0,0 +1,26 @@
+package dayone;
+
+public class CharDemo {
+	public static void main(String[] args) {
+		
+		char ch = 'a';
+		System.out.println(ch);
+
+		
+		char ch1 = 85;
+		System.out.println(ch1);
+
+		
+		char var1 = '\u00A7';
+		System.out.println(var1);
+
+		
+		char var2 = '\u20AC';
+		System.out.println(var2);
+
+		
+		int a = 'S';
+		System.out.println(a);
+	}
+
+}
