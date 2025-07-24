131628010bfe69f25b03cbcdfedcbd14c5eb56c9
Asus <Asus@TUF-F15>
2025-07-24 19:50:43 +0530

    this is my first inheritance

---
+++ C2TC_CoreJavaProgram/src/com/tnsif/dayfive/multilevelinheritance/vehicle/Car.java
@@ -0,0 +1,18 @@
+package com.tnsif.dayfive.multilevelinheritance.vehicle;
+
+public class Car {
+	public Car() {
+		System.out.println("Class Car");
+	}
+
+	public void vehicleType1() {
+		System.out.println("Vehicle Type: Car");
+	}
+
+	public void vehicleType() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+	
