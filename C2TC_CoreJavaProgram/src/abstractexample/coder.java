e0973f003d03ab896359815f6a432829fe0fea85
Asus <Asus@TUF-F15>
2025-07-28 15:55:49 +0530

    this is my Program to demonstrate abstraction

--- C2TC_CoreJavaProgram/src/abstractexample/coder.java
+++ C2TC_CoreJavaProgram/src/abstractexample/coder.java
@@ -2,7 +2,11 @@ package abstractexample;
 
 public abstract class coder {
 	protected  static final String  wakeup="5 Am";
+	
+	//Abstract methord declared
 	abstract public void rules();
+	
+	//concrete methord
 	public void show()
 	{
 		System.out.println("To become a coder, Early morning practical needed");
