479e6d583e8692ae5d0bb9c810e2eac8a6e46446
Asus <Asus@TUF-F15>
2025-07-30 13:37:58 +0530

    assignment 5

--- C2TC_CoreJavaProgram/src/assignment4/AirIndia.java
+++ C2TC_CoreJavaProgram/src/assignment4/AirIndia.java
@@ -1,42 +1,33 @@
-package assignment5;
+package assignment4;
 
-// AirIndia.java
 public class AirIndia implements Airfare {
     private int hours;
     private double costPerHour;
 
-    // Default constructor
     public AirIndia() {
-        this.hours = 0;
-        this.costPerHour = 0.0;
     }
 
-    // Parameterized constructor
     public AirIndia(int hours, double costPerHour) {
         this.hours = hours;
         this.costPerHour = costPerHour;
     }
 
-    // Getters
     public int getHours() {
         return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
     }
 
     public double getCostPerHour() {
         return costPerHour;
     }
 
-    // Setters
-    public void setHours(int hours) {
-        this.hours = hours;
-    }
-
     public void setCostPerHour(double costPerHour) {
         this.costPerHour = costPerHour;
     }
 
-    // Implementing method from Airfare interface
-    @Override
     public double calculateAmount() {
         return hours * costPerHour;
     }
--- C2TC_CoreJavaProgram/src/assignment5/Airfare.java
+++
@@ -1,6 +0,0 @@
-package assignment5;
-
-// Airfare.java (Interface)
-public interface Airfare {
-    double calculateAmount();  // common method for all airlines
-}
